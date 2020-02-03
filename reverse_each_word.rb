0be30ca34c3d0f1df570622c97750b19e6032ac0
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-03 18:35:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(phrases)
    array = phrases.split
    final []
    array.collect do |phrase|
-     string = word.reverse
+     final << word.reverse
      
    end
    puts phrase
